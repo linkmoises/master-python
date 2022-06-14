edd092b52105225076ae6512c144eace75c6f07b
Moisés Serrano <linkmoises@gmail.com>
2022-06-14 10:10:54 -0500

    añadido trabajo clase 9 variables

---
+++ 009-variables.py
@@ -0,0 +1,16 @@
+texto = "Master en python"
+texto2 = "con Moisés"
+print(texto)
+print(texto2)
+
+numero = 42
+decimal = 3.14
+
+print(numero)
+print(decimal)
+
+numero = 60
+
+print(numero)
+
+# como python es un lenguaje debilmente tipado, se pueden redifinir variables con solo volver a redefinirlas
